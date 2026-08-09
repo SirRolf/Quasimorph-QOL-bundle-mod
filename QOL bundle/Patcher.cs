1355057b7f0c19e8cebe15c8bba4484edc1893e6
SirRolf <flomo123456789@gmail.com>
2026-08-09 09:53:15 +0200

    Added the project

---
+++ QOL bundle/Patcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using MGSC;
+
+namespace QOL_bundle
+{
+	public class Patcher
+	{
+		[Hook(ModHookType.AfterConfigsLoaded)]
+		public static void AfterConfigsLoaded(IModContext context)
+		{
+			Harmony harmony = new Harmony("Test_Mod");
+			harmony.PatchAll();
+		}
+	}
+}
