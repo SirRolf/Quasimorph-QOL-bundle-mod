7f3b57e15e2d4d980bf3ad041d303dfb293194a8
SirRolf <flomo123456789@gmail.com>
2026-08-11 13:25:09 +0200

    Now using the ConfirmMagnumUpgradeWindow so it can only happen there. Also using the perkRecord variable to get the ItemID

--- src/Features/ClickForStock/ClickForStockPatch.cs
+++ src/Features/ClickForStock/ClickForStockPatch.cs
@@ -3,12 +3,15 @@ using MGSC;
 
 namespace QOL_bundle.Features.ClickForStock
 {
-    [HarmonyPatch(typeof(TooltipItemIcon),nameof(TooltipItemIcon.Initialize))]
+    [HarmonyPatch(typeof(ConfirmMagnumUpgradeWindow),nameof(ConfirmMagnumUpgradeWindow.Configure))]
     public static class ClickForStockPatch
     {
-        public static void Prefix(string itemId, TooltipItemIcon __instance)
+        public static void Postfix(MagnumPerkRecord perkRecord,ConfirmMagnumUpgradeWindow __instance)
         {
-	        __instance.gameObject.AddComponent<ClickItemForStockBehaviour>().Configure(itemId);//probably use MGSC.CommonButton
+            for (int i = 0; i < __instance._rewardsGrid._rewardsRoot.childCount; i++)
+            {
+                __instance._rewardsGrid._rewardsRoot.GetChild(i).gameObject.AddComponent<ClickItemForStockBehaviour>().Configure(perkRecord.UpgradePrice[i]);//using index here can be somewhat unsafe but i don't know of an alternative. Feel free to suggest one
+            }
         }
     }
 }
