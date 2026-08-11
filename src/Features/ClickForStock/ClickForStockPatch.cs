d12c65366f53b30eca2340b0c45c70f4d89dfc60
SirRolf <flomo123456789@gmail.com>
2026-08-11 18:04:23 +0200

    Had to switch things aruond again to make sure we always grab the right ID

--- src/Features/ClickForStock/ClickForStockPatch.cs
+++ src/Features/ClickForStock/ClickForStockPatch.cs
@@ -1,16 +1,19 @@
 ﻿using HarmonyLib;
 using MGSC;
+using UnityEngine;
 
 namespace QOL_bundle.Features.ClickForStock
 {
     [HarmonyPatch(typeof(ConfirmMagnumUpgradeWindow),nameof(ConfirmMagnumUpgradeWindow.Configure))]
     public static class ClickForStockPatch
     {
-        public static void Postfix(MagnumPerkRecord perkRecord,ConfirmMagnumUpgradeWindow __instance)
+        public static void Postfix(ConfirmMagnumUpgradeWindow __instance)
         {
             for (int i = 0; i < __instance._rewardsGrid._rewardsRoot.childCount; i++)
             {
-                __instance._rewardsGrid._rewardsRoot.GetChild(i).gameObject.AddComponent<ClickItemForStockBehaviour>().Configure(perkRecord.UpgradePrice[i]);//using index here can be somewhat unsafe but i don't know of an alternative. Feel free to suggest one
+                GameObject gameObject = __instance._rewardsGrid._rewardsRoot.GetChild(i).gameObject;//Wish i could add all ItemTooltipHandlers in a list in the RewardsGrid
+                string itemId = gameObject.GetComponent<ItemTooltipHandler>()._itemRecord.Id;
+                gameObject.AddComponent<ClickItemForStockBehaviour>().Configure(itemId);
             }
         }
     }
