da84ec02c3c5f63658354775263b608f3e04d756
SirRolf <flomo123456789@gmail.com>
2026-08-12 23:05:40 +0200

    Initial try. It seems to get hung up on the consumeItem method

---
+++ src/Features/InventoryHotkeys/InventoryHotkeysBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MGSC;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace QOL_bundle.Features.InventoryHotkeys
+{
+	public class InventoryHotkeysBehaviour : MonoBehaviour ,IPointerEnterHandler, IPointerExitHandler
+	{
+		private bool _isHovering;
+		private BasePickupItem _item;
+
+		public InventoryHotkeysBehaviour Configure(BasePickupItem item)
+		{
+			_item = item;
+			return this;
+		}
+		
+		private void Update()
+		{
+			if (_isHovering && InputHelper.GetKeyDown(KeyCode.E))//probably should change to a keybind but not sure how to do that yet
+			{
+				Plugin.Logger.Log($"Trying to consume: {_item.Id}");
+				ItemInteractionSystem.ConsumeItem(_item);
+			}
+		}
+
+		public void OnPointerEnter(PointerEventData eventData) => _isHovering = true;
+
+		public void OnPointerExit(PointerEventData eventData) => _isHovering = false;
+	}
+}
---
+++ src/Features/InventoryHotkeys/InventoryHotkeysPatch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using MGSC;
+
+namespace QOL_bundle.Features.InventoryHotkeys
+{
+	[HarmonyPatch(typeof(ItemSlot),nameof(ItemSlot.Initialize))]
+	public class InventoryHotkeysPatch
+	{
+		private static readonly Dictionary<ItemSlot, InventoryHotkeysBehaviour> _behaviourInstances = new Dictionary<ItemSlot, InventoryHotkeysBehaviour>();
+		
+		public static void Postfix(ItemSlot __instance)
+		{
+			if (_behaviourInstances.ContainsKey(__instance)) return;//gotta do some cleanup here
+			_behaviourInstances.Add(__instance, __instance.gameObject.AddComponent<InventoryHotkeysBehaviour>().Configure(__instance.Item));
+			Plugin.Logger.Log("InventoryHotkeysPatch applied");
+		}
+	}
+}
