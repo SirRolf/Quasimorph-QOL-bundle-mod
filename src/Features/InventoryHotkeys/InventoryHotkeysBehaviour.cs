8ed6c64b878ee9b7092227056409cd254d7c806a
SirRolf <flomo123456789@gmail.com>
2026-08-13 10:12:26 +0200

    Correctly consumes items but seems to consume non existant items too.

--- src/Features/InventoryHotkeys/InventoryHotkeysBehaviour.cs
+++ src/Features/InventoryHotkeys/InventoryHotkeysBehaviour.cs
@@ -7,12 +7,16 @@ namespace QOL_bundle.Features.InventoryHotkeys
 {
 	public class InventoryHotkeysBehaviour : MonoBehaviour ,IPointerEnterHandler, IPointerExitHandler
 	{
-		private bool _isHovering;
 		private BasePickupItem _item;
+		private InventoryScreen _inventoryScreen;
+		
+		private bool _isHovering;
 
-		public InventoryHotkeysBehaviour Configure(BasePickupItem item)
+		public InventoryHotkeysBehaviour Configure(BasePickupItem item, InventoryScreen inventoryScreen)
 		{
+			Plugin.Logger.Log($"item: {item},InventoryScreen: {inventoryScreen}");
 			_item = item;
+			_inventoryScreen =  inventoryScreen;
 			return this;
 		}
 		
@@ -20,8 +24,9 @@ namespace QOL_bundle.Features.InventoryHotkeys
 		{
 			if (_isHovering && InputHelper.GetKeyDown(KeyCode.E))//probably should change to a keybind but not sure how to do that yet
 			{
-				Plugin.Logger.Log($"Trying to consume: {_item.Id}");
-				ItemInteractionSystem.ConsumeItem(_item);
+				UsableItemComponent usableItemComponent = _item.Comp<UsableItemComponent>();
+				Plugin.Logger.Log($"Trying to consume: {_item.Id} usable: {_item.IsUsable}. got component: {usableItemComponent}");
+				_inventoryScreen.InteractWithCharacter(_item,true);
 			}
 		}
 
--- src/Features/InventoryHotkeys/InventoryHotkeysPatch.cs
+++ src/Features/InventoryHotkeys/InventoryHotkeysPatch.cs
@@ -4,15 +4,29 @@ using MGSC;
 
 namespace QOL_bundle.Features.InventoryHotkeys
 {
+	public static class InventoryHotkeysPatch
+	{
+		public static InventoryScreen InventoryScreen;
+	}
+
+	[HarmonyPatch(typeof(InventoryScreen), nameof(InventoryScreen.Awake))]
+	public class InventoryHotkeysPatchInventory
+	{
+		public static void Postfix(InventoryScreen __instance)
+		{
+			InventoryHotkeysPatch.InventoryScreen = __instance;
+		}
+	}
+	
 	[HarmonyPatch(typeof(ItemSlot),nameof(ItemSlot.Initialize))]
-	public class InventoryHotkeysPatch
+	public class InventoryHotkeysPatchItemSlot
 	{
 		private static readonly Dictionary<ItemSlot, InventoryHotkeysBehaviour> _behaviourInstances = new Dictionary<ItemSlot, InventoryHotkeysBehaviour>();
 		
 		public static void Postfix(ItemSlot __instance)
 		{
 			if (_behaviourInstances.ContainsKey(__instance)) return;//gotta do some cleanup here
-			_behaviourInstances.Add(__instance, __instance.gameObject.AddComponent<InventoryHotkeysBehaviour>().Configure(__instance.Item));
+			_behaviourInstances.Add(__instance, __instance.gameObject.AddComponent<InventoryHotkeysBehaviour>().Configure(__instance.Item, InventoryHotkeysPatch.InventoryScreen));
 			Plugin.Logger.Log("InventoryHotkeysPatch applied");
 		}
 	}
